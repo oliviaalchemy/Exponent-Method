83d6801ea39e92f19fc95d4e8672adc000c5b06b
oliviaalchemy <olivia@alchemytechs.com>
2019-08-07 14:26:02 +0100

    Add project files.

---
+++ Exponent Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exponent_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(3, 2));
+            Console.ReadLine();
+        }
+        static int GetPow(int baseNum, int powNum) //GetPow only works for positive number powers
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum; 
+            }
+
+            return result; //will read 9
+        }
+    }
+}
